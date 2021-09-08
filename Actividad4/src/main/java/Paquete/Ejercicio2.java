c421b703578564e37651ab3597034302d70d8b75
brandonm2911 <bamunozr@ucundinamarca.edu.co>
2021-09-08 18:47:20 -0500

    add

---
+++ Actividad4/src/main/java/Paquete/Ejercicio2.java
@@ -0,0 +1,21 @@
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int numero1 = 12, numero2 = 2;
+        double division, modulo, operacion;
+        int suma, resta, multiplicacion;
+        
+        operacion = 9/35-3+82;
+        System.out.println("El resultado de la operacion es = " + operacion);
+        division = numero1/numero2;
+        System.out.println("El resultado de la  division es = " + division);
+        suma = numero1 + numero2; 
+        System.out.println("El resultado de la suma es = " + suma);
+        resta = numero1 - numero2;
+        System.out.println("El resultado de la resta es = " + resta);
+        multiplicacion = numero1 * numero2;
+        System.out.println("El resultado de la multiplicacioon es = " + multiplicacion);
+        modulo = numero1 % numero2;
+        System.out.println("El resutado del modulo es = " + modulo);
+    }
+}
